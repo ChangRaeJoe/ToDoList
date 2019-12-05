ec66a062fd7eeb7d586ea8d22406b59e039e4191
ChangRaeJoe <ckdfo1995@naver.com>
2019-12-06 00:08:01 +0900

    add weather.js
    weather file add.

---
+++ weather.js
@@ -0,0 +1,68 @@
+const API_KEY = null;  //your API Key (openWeather api .etc)
+const COORDS = 'coords';
+const weather = document.querySelector('.js-weather');
+
+function isApiKey(){
+    return (API_KEY===null ? false: true);
+}
+
+function getWeather(lat, log){
+    if(!isApiKey()){
+        console.log("API KEY doesn't exist");
+        return;
+    }
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+        ).then(function(response){
+            return response.json();
+        }).then(function(json){
+            const temp = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temp}도 @ ${place}`;
+        });
+    
+}
+
+function saveCoords(coord){
+    const parsed = JSON.stringify(coord);
+    localStorage.setItem(COORDS,parsed);
+}
+
+function handleGeoSuccess(pos){
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+    const coord = {
+        latitude,
+        longitude
+    };
+
+    //��ġ ����
+    saveCoords(coord);
+    //������ü�� api��û
+    getWeather(latitude, longitude);
+    //������ ������ ����
+}
+
+function handleGeoError(){
+    console.log("not access current geo position");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else{
+        const parsed = JSON.parse(loadedCoords);
+        getWeather(parsed.latitude, parsed.longitude);
+    }
+}
+
+init();
+function init(){
+    console.log("hello weather");
+    loadCoords();
+}
